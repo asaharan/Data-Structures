c742ddcbf09643ee94a3d1a373ada24b38b0a9f6
Amit Saharan <asaharan812@gmail.com>
2015-07-31 15:32:04 +0530

    GCD completed

--- Algorithms/gcd.py
+++ Algorithms/gcd.py
@@ -1,2 +1,13 @@
-def fun(a,b)
-	reutrn a%b
+def gcd(f,s):# assuming f > s
+	while s!=0:
+		t=s
+		s=f%s
+		f=t
+	return f
+f=input('First number: ')
+s=input('Second number: ')
+if f<s:
+	t=f
+	f=s
+	s=t
+print "GCD of %d and %d is %d"%( f, s,gcd(f,s))
